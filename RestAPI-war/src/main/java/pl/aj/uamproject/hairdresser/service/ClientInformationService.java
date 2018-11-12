7ef2f1344f2b259a97820fea586ed538134c1ef0
KamilSobien <kamil.sobien@gmail.com>
2018-11-13 00:47:00 +0100

    renamed controller, added inMemoryDatabase, added mockDatabase, added getById/PhoneNumber/LastName

--- RestAPI-war/src/main/java/pl/aj/uamproject/hairdresser/service/ClientInformationService.java
+++ RestAPI-war/src/main/java/pl/aj/uamproject/hairdresser/service/ClientInformationService.java
@@ -4,11 +4,39 @@ import pl.aj.uamproject.hairdresser.dto.SimpleClientInfoDTO;
 import pl.aj.uamproject.hairdresser.model.Client;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ClientInformationService {
-    public ArrayList<SimpleClientInfoDTO> getAllClients() {
-        ArrayList<SimpleClientInfoDTO> clients = new ArrayList<>();
-        clients.add(new SimpleClientInfoDTO(1, "Jan", "Kowalski", "698488394"));
-        return clients;
+    private final Set<SimpleClientInfoDTO> database = new HashSet<>();
+
+    public ClientInformationService(){
+        mockDatabase();
+    }
+
+    private void mockDatabase(){
+        database.add(new SimpleClientInfoDTO(1, "Jan", "Kowalski", "698488394"));
+        database.add(new SimpleClientInfoDTO(2, "Robert", "Nowak", "123456789"));
+        database.add(new SimpleClientInfoDTO(3, "Sebastian", "Chmielewski", "569845316"));
+        database.add(new SimpleClientInfoDTO(4, "Piotr", "Gajewski", "745896321"));
+        database.add(new SimpleClientInfoDTO(5, "Wiktoria", "Nowak", "986532147"));
+    }
+
+    public List<SimpleClientInfoDTO> getAllClients() {
+        return new ArrayList<>(database);
+    }
+
+    public SimpleClientInfoDTO getClientById(long id){
+        return database.stream().filter(client -> client.getId() == id).findFirst().get();
+    }
+
+    public SimpleClientInfoDTO getClientByPhoneNumber(String phoneNumber){
+        return database.stream().filter(client -> client.getPhoneNumber().equals(phoneNumber)).findFirst().get();
+    }
+
+    public SimpleClientInfoDTO getClientByLastName(String lastName){
+        return database.stream().filter(client -> client.getLastName().equals(lastName)).findFirst().get();
     }
 }
